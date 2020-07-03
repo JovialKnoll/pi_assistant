a7261265c1093281fb773a65bbe6ac316be539b7
Brett Kaplan <BrettCKaplan@gmail.com>
2020-07-03 17:45:00 -0400

    less out

--- file-speak.py
+++ file-speak.py
@@ -6,11 +6,11 @@ import subprocess
 TEMP_FILE_NAME = 'temp.mp3'
 
 def speak(text):
-    command = 'wget -q --timeout=5 -U Mozilla -O "{}"'\
+    command = 'wget --quiet --timeout=5 -U Mozilla -O "{}"'\
         ' "https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q={}"'\
         .format(TEMP_FILE_NAME, text)
     exit_code = subprocess.call(command, shell=True)
-    play_command = 'vlc --play-and-exit {}'.format(TEMP_FILE_NAME)
+    play_command = 'vlc --quiet -I dummy --play-and-exit {}'.format(TEMP_FILE_NAME)
     if exit_code != 0:
         play_command = 'espeak -v en-us+f3 -g 5 "{}"'.format(text)
     subprocess.call(play_command, shell=True)
