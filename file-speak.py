62058f2c792831324eee1ed46a6d094031f58b20
JovialKnoll <BrettCKaplan@gmail.com>
2020-07-03 15:50:52 -0400

    fixing syntax

--- file-speak.py
+++ file-speak.py
@@ -3,15 +3,13 @@
 import sys
 import subprocess
 
-def speak_online(text) {
+def speak_online(text):
     command = 'wget -q -U Mozilla -O "temp.mp3" "'
     command += 'https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q='
     command += text
     command += '"'
     #play_command = 'vlc --play-and-exit "https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q="'
     subprocess.call(command, shell=True)
-}
-#say $*
 
 def speak(text):
     command = 'espeak -v en-us+f3 -g 5 "{}"'.format(text)
