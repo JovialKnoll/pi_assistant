b7e8d24935abd611cad0115cf4908319862d98e5
JovialKnoll <BrettCKaplan@gmail.com>
2020-07-03 17:05:49 -0400

    cleanup with string format

--- file-speak.py
+++ file-speak.py
@@ -3,13 +3,15 @@
 import sys
 import subprocess
 
+TEMP_FILE_NAME = 'temp.mp3'
+
 def speak_online(text):
-    command = 'wget -q -U Mozilla -O "temp.mp3" "'
-    command += 'https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q='
-    command += text
-    command += '"'
+    command = 'wget -q --timeout=5 -U Mozilla -O "{}"'\
+        ' "https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q={}"'\
+        .format(TEMP_FILE_NAME, text)
+    exit_code = subprocess.call(command, shell=True)
+    print(exit_code)
     #play_command = 'vlc --play-and-exit "https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q="'
-    subprocess.call(command, shell=True)
 
 def speak(text):
     command = 'espeak -v en-us+f3 -g 5 "{}"'.format(text)
@@ -17,6 +19,6 @@ def speak(text):
 
 #speak("test text")
 
-speak_online("it's ya boy with some test text")
+speak_online("good morning sir")
 
 sys.exit()
