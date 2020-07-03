d98a81063547c2fb7de779b466663774b0034b1f
JovialKnoll <BrettCKaplan@gmail.com>
2020-07-03 15:49:25 -0400

    grabbing online tts output

--- file-speak.py
+++ file-speak.py
@@ -3,16 +3,22 @@
 import sys
 import subprocess
 
-#say() {
-#    local IFS=+;
-#    vlc --play-and-exit "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en";
-#}
+def speak_online(text) {
+    command = 'wget -q -U Mozilla -O "temp.mp3" "'
+    command += 'https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q='
+    command += text
+    command += '"'
+    #play_command = 'vlc --play-and-exit "https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q="'
+    subprocess.call(command, shell=True)
+}
 #say $*
 
 def speak(text):
     command = 'espeak -v en-us+f3 -g 5 "{}"'.format(text)
     subprocess.call(command, shell=True)
 
-speak("test text")
+#speak("test text")
+
+speak_online("it's ya boy with some test text")
 
 sys.exit()
