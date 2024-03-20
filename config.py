1ff8ed2da3ba12b6b377fc484c1e387306f12030
Brett Kaplan <BrettCKaplan@gmail.com>
2024-03-20 11:42:12 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+import json
+
+_SRC_DIRECTORY = os.path.dirname(__file__)
+_CONFIG_FILE = os.path.join(SRC_DIRECTORY, 'config.json')
+_config = None
+with open(_CONFIG_FILE) as file:
+    _config = json.load(file)
+
+KEY_IPINFO = _config['key_ipinfo']
+KEY_OPENWEATHERMAP = _config['key_openweathermap']
+CONFIG_CITY = _config['config_city']
